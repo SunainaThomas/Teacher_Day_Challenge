ef4936a5eb4bd4e794395bf677a2e94871641e1b
SunainaThomas <sunainathomasdelhi@gmail.com>
2025-07-26 20:01:31 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0; // Initialize count
+        for (int i = 0; i < nums.size(); i++) {
+            int digitcount = 0;
+            int num = nums[i]; // Use a temporary variable to avoid modifying input
+            while (num > 0) {
+                num /= 10;
+                digitcount++;
+            }
+            if (digitcount % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
